b3277170f6b61432ccf93c3d70c6ba3bb056625c
MathiasRobbensPXL <Mathias.Robbens@student.pxl.be>
2024-11-14 13:55:04 +0100

    MasterMind-04 Kleur Kiezen

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Reflection.Emit;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -40,14 +41,42 @@ namespace Mastermind
 
         private void ComboBoxes()
         {
+            comboBox1.ItemsSource = colors;
+            comboBox2.ItemsSource = colors;
+            comboBox3.ItemsSource = colors;
+            comboBox4.ItemsSource = colors;
 
-            foreach (string colors in colors)
-            {
-                comboBox1.Items.Add(colors);
-                comboBox2.Items.Add(colors);
-                comboBox3.Items.Add(colors);
-                comboBox4.Items.Add(colors);
-            }
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Als er een kleur is geselecteerd in combobox 1, toon deze in label 1
+            //if (comboBox1.SelectedItem != null)
+            //label1.Content = comboBox1.SelectedItem.ToString();
+
+            // Doe hetzelfde voor de andere comboboxen en labels
+            //if (comboBox2.SelectedItem != null)
+            //label2.Content = comboBox2.SelectedItem.ToString();
+            //if (comboBox3.SelectedItem != null)
+            //label3.Content = comboBox3.SelectedItem.ToString();
+            //if (comboBox4.SelectedItem != null)
+            //label4.Content = comboBox4.SelectedItem.ToString();
+
+            //Deze code gaf enkel de naam weer van de kleur in het bijhorende label, even bijwerken.
+
+            if (comboBox1.SelectedItem != null)
+                label1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(comboBox1.SelectedItem.ToString()));
+            if (comboBox2.SelectedItem != null)
+                label2.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(comboBox2.SelectedItem.ToString()));
+            if (comboBox3.SelectedItem != null)
+                label3.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(comboBox3.SelectedItem.ToString()));
+            if (comboBox4.SelectedItem != null)
+                label4.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(comboBox4.SelectedItem.ToString()));
+
+        }
+
+        private void validateButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
